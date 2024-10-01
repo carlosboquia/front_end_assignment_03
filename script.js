0fe472561d1f7ec1f59b56c3a90e1df81c29d87c
Carlos Boquia <cboquia@academic.rrc.ca>
2024-10-01 15:39:47 -0500

    Optimized JavaScript by adding comments and cleaning up code. Ready to launch

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// Start of the game
 let currentState = "start";
 
+// Function to update story from choices.
 function renderQuestion() {
+    
+    // Grabs the elements from the HTML 
     const questionContainer = document.getElementById("question");
     const answersContainer = document.getElementById("answers");
 
-
+    // The multiple choices within the game based off the const Story variable
     switch (currentState) {
         case "start":
             console.log("game start")
@@ -53,23 +57,27 @@ function renderQuestion() {
             console.log("Game Error restart page")
     }
 
-
+    // Grabs the question based off the players choices
     questionContainer.textContent = story[currentState].question;
+    
+    // returns an empty string (clearing previous choices) from when choices are made. 
     answersContainer.textContent = "";
     
+    // A for loop that refreshes everytime a choice is chosen. adds new button based off story choice
     for (let i = 0; i < story[currentState].choice.length; i++) {
         const choice = story[currentState].choice[i];
         const button = addAnswerButton(choice);
         answersContainer.appendChild(button);
-    }
-    
+    }    
 }
 
+// Creates buttons on page
 function addAnswerButton(choice) {
     const button = document.createElement("button");
     console.log("Button was created!")
     button.textContent = choice.text;
 
+    // An eventlistener that updates the current state based of the choice that has been clicked
     button.addEventListener('click', () => {
         currentState = choice.nextState;
         console.log("Button was clicked!")
@@ -78,48 +86,41 @@ function addAnswerButton(choice) {
     return button;
 }
 
-
+// The story of the game that holds all the different pathways. 
 const story = {
     start: {
-        question:"Choose your destiny",
+        question:"Choose your destiny.",
         choice: [
             { text: "Red Pill", nextState: "redpill"},
             { text: "Blue Pill", nextState: "bluepill"},
         ]
     },
-
     redpill: {
-        question:"Enjoy paradise",
+        question:"Enjoy your paradise. GAME OVER",
         choice: [
             { text: "Restart Game", nextState: "start"},
-            
         ]
     },
-
     bluepill: {
-        question:"Choose your Faction",
+        question:"Choose a faction",
         choice: [
             { text: "Join the Resistance", nextState: "resistance"},
             { text: "Join the Robots", nextState: "robots"},
         ]
     },
-
     resistance: {
-        question:"Welcome to the resistance!",
+        question:"Welcome to the Resistance!",
         choice: [
             { text: "Arm up to fight!", nextState: "fight"},
-            { text: "Stay within the community", nextState: "stay"},
-            
+            { text: "Stay at Home Base", nextState: "stay"}, 
         ]
     },
-
     robots: {
-        question:"Enjoy paradise",
+        question:"We are at war with the robots. GAME OVER",
         choice: [
             { text: "Restart Game", nextState: "start"},
         ]
     },
-
     fight: {
         question:"Go into battle! How would you like to proceed?",
         choice: [
@@ -127,15 +128,12 @@ const story = {
             { text: "Go in quiet", nextState: "quiet"},
         ]
     },
-
     stay: {
-        question:"You didn't want to fight. The robots have overrun us! Enjoy paradise.",
+        question:"You didn't want to fight. The robots have overrun us! GAME OVER",
         choice: [
             { text: "Restart Game", nextState: "start"},
         ]
     },
- 
- 
     loud: {
         question:"Who will you fight first?",
         choice: [
@@ -143,49 +141,43 @@ const story = {
             { text: "Robot Minions", nextState: "loudminions"},
         ]
     },
- 
     quiet: {
         question:"Which area do you want to sabotage?",
         choice: [
             { text: "The Main Frame", nextState: "quietmainframe"},
-            { text: "Engineers Brain", nextState: "quietengineerbrain"},
+            { text: "The Engineers Brain", nextState: "quietengineerbrain"},
         ]
     },
- 
     loudheadrobot: {
-        question:"You did not sabotage the engineers, you have got overrun. Enjoy Paradise",
+        question:"You did not sabotage the engineers, you have got overrun. GAME OVER",
         choice: [
             { text: "Restart Game", nextState: "start"},
         ]
     },
- 
     loudminions: {
-        question:"The minions are too strong for our forces. They have overrun us. Enjoy Paradise",
+        question:"You have eliminated the minions. But the engineers have taken over our camp. GAME OVER",
         choice: [
             { text: "Restart Game", nextState: "start"},
         ]
     },
- 
     quietmainframe: {
-        question:"You have been caught by robot security. The Robots Have Won..Enjoy Paradise.",
+        question:"You have been caught by robot security. The Robots Have Won. GAME OVER",
         choice: [
             { text: "Restart Game", nextState: "start"},
         ]
     },
-
     quietengineerbrain: {
         question:"You made it to the brain! Disable the brain and win for the resistance!",
         choice: [
-            { text: "HACK THE BRAIN!", nextState: "end"},
+            { text: "DISABLE THE ENGINEERS BRAIN!", nextState: "end"},
         ]
     },
-
     end: {
         question:"The Robots have been disabled. The resistance have won!",
         choice: [
-            { text: "Welcome to paradise?", nextState: "start"},
+            { text: "YOU WIN!", nextState: "start"},
         ]
     },
-
 }
+// function to run the whole code
 renderQuestion();
